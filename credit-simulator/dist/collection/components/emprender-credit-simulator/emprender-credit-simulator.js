28b3e00d7b3622229fffa3af310e41a35cd1c069
Andres Betin <andresdavidbetin@gmail.com>
2021-05-25 18:46:22 -0500

    Update relative fonts for credit simulator

--- credit-simulator/dist/collection/components/emprender-credit-simulator/emprender-credit-simulator.js
+++ credit-simulator/dist/collection/components/emprender-credit-simulator/emprender-credit-simulator.js
@@ -8,11 +8,11 @@ export class EmprenderCreditSimulator {
   }
   async componentWillLoad() {
     await loadCSS("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@400;500&family=Varela+Round&display=swap");
-    await loadCSS("./assets/emprender-icons.css");
     await loadScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', 'popperjs', 'text/javascript');
     await loadScript('https://code.jquery.com/jquery-3.6.0.min.js', 'jquery', 'text/javascript');
     await loadScript('https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js', 'bootstrap', 'text/javascript');
     await loadScript('https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/11.0.2/bootstrap-slider.min.js', 'bootstrap-slider', 'text/javascript');
+    await loadScript('https://imaginemos-ae.github.io/com.emprender.FrontEnd-demo/components-library/dist/emprender-components-library/emprender-components-library.esm.js', 'emprender-components-library', 'module');
   }
   componentDidLoad() {
     const element = this.host.shadowRoot.querySelectorAll('[data-toggle="tooltip"]');
@@ -45,7 +45,7 @@ export class EmprenderCreditSimulator {
             h("span", null, "$485.443")),
           h("p", { class: "small" }, "Este valor corresponde a una simulaci\u00F3n de tu cr\u00E9dito seg\u00FAn los datos seleccionados por ti."),
           h("div", { class: "details" }, this.currencyValues.map(_currencyValue => h("emprender-cs-item", { text: _currencyValue.label, subText: _currencyValue.subLabel, value: _currencyValue.value, space: _currencyValue.space }, _currencyValue.tooltip &&
-            h("a", { "data-toggle": "tooltip", "data-placement": "top", title: _currencyValue.tooltip, class: "tooltipWhite icon-info" })))),
+            h("emprender-cl-icon", { "data-toggle": "tooltip", "data-placement": "top", title: _currencyValue.tooltip, class: "tooltipWhite", icon: "info" })))),
           h("div", { class: "contcenter" },
             h("button", { class: "button medium purple" }, "Solicita tu cr\u00E9dito"))))));
   }
