70a78303196591e07f64e382a5762899492f4077
melyssaS <melyssasolanod@gmail.com>
2021-07-27 20:41:46 -0500

    user-biometric

--- user-biometrics/dist/types/components/emprender-ub-home/emprender-ub-home.d.ts
+++ user-biometrics/dist/types/components/emprender-ub-home/emprender-ub-home.d.ts
@@ -2,5 +2,7 @@ import { EventEmitter } from '../../stencil-public-runtime';
 export declare class EmprenderUbHome {
   continue: EventEmitter<void>;
   requestToADO(): void;
+  detectDevice(): boolean;
+  getInformationItem(): any;
   render(): any;
 }
