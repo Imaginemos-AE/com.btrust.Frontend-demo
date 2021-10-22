fccef9d0a9f9b3a4b90bc98c62a0d722fe3f9c70
melyssaS <melyssasolanod@gmail.com>
2021-10-22 10:55:26 -0500

    fix biometric

--- user-biometrics/dist/cjs/emprender-ub-failure_6.cjs.entry.js
+++ user-biometrics/dist/cjs/emprender-ub-failure_6.cjs.entry.js
@@ -216,16 +216,18 @@ const EmprenderUbHome$1 = class {
   }
   requestToADO() {
     if (state.adoCofiguration) {
-      const { url, redirectUrl, key, projectName, product } = state.adoCofiguration;
-      const _redirect = redirectUrl !== null && redirectUrl !== void 0 ? redirectUrl : location.href;
-      const adoURL = `${url}?callback=${_redirect}&key=${key}&projectName=${projectName}&product=${product}`;
-      window.location.assign(adoURL);
-      // if (state.adoCofiguration) {
-      //   const { url, redirectUrl, key, projectName, product, documentType, documentNumber } = state.adoCofiguration;
-      //   const _redirect = redirectUrl ?? location.href;
-      //   const adoURL = `${url}?callback=${_redirect}&key=${key}&projectName=${projectName}&product=${product}&documentType=${documentType}&identificationNumber=${documentNumber}`;
-      //   window.location.assign(adoURL);
-      // }
+      if (state.adoCofiguration.hasOwnProperty("documentType")) {
+        const { url, redirectUrl, key, projectName, product, documentType, documentNumber } = state.adoCofiguration;
+        const _redirect = redirectUrl !== null && redirectUrl !== void 0 ? redirectUrl : location.href;
+        const adoURL = `${url}?callback=${_redirect}&key=${key}&projectName=${projectName}&product=${product}&documentType=${documentType}&identificationNumber=${documentNumber}`;
+        window.location.assign(adoURL);
+      }
+      else {
+        const { url, redirectUrl, key, projectName, product } = state.adoCofiguration;
+        const _redirect = redirectUrl !== null && redirectUrl !== void 0 ? redirectUrl : location.href;
+        const adoURL = `${url}?callback=${_redirect}&key=${key}&projectName=${projectName}&product=${product}`;
+        window.location.assign(adoURL);
+      }
     }
   }
   detectDevice() {
