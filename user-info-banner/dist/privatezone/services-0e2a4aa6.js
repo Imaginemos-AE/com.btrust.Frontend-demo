dd870ac0081d5aee6b451ea2d48732e49f2647e3
Melyssa Solano Diaz <melyssasolanod@gmail.com>
2022-05-31 20:40:27 -0500

    FIX dist user-info-banner

--- user-info-banner/dist/privatezone/services-0e2a4aa6.js
+++ user-info-banner/dist/privatezone/services-0e2a4aa6.js
@@ -1,7 +1,7 @@
-export function formatNumber(num, signSpace = false) {
+function formatNumber(num, signSpace = false) {
   return `$${signSpace ? ' ' : ''}${num === null || num === void 0 ? void 0 : num.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")}`;
 }
-export function loadScript(url, id, type) {
+function loadScript(url, id, type) {
   return new Promise(resolve => {
     document.body.appendChild(Object.assign(document.createElement('script'), {
       type: type,
@@ -13,7 +13,7 @@ export function loadScript(url, id, type) {
     }));
   });
 }
-export function loadCSS(url) {
+function loadCSS(url) {
   return new Promise(resolve => {
     const links = document.getElementsByTagName('link');
     const exist = Array.from(links).some(_link => _link.href === url);
@@ -29,3 +29,28 @@ export function loadCSS(url) {
     }
   });
 }
+
+const keycloakConfig = {
+  url: 'https://credito.muii.com.co/auth',
+  realm: 'muii',
+  clientId: 'muii-pz-user-info'
+};
+class AuthKeycloakService {
+  constructor() {
+    const _keycloakClass = window["Keycloak"];
+    this.keycloakInstance = _keycloakClass(keycloakConfig);
+  }
+  async build() {
+    return this.keycloakInstance.init({
+      onLoad: "check-sso",
+      silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+    }).then((auth) => {
+      if (!auth) {
+        throw new Error("Not authenticated");
+      }
+      return this.keycloakInstance;
+    });
+  }
+}
+
+export { AuthKeycloakService as A, loadScript as a, loadCSS as l };
